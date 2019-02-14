69dd49bad7ed3acc4438762feafb178cbbedc61a
sajad1993gh <sajad1993gh@gmail.com>
2019-02-14 15:46:01 -0500

    completed

--- src/components/button.js
+++ src/components/button.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 class Button extends Component {
   render() {
     return(
-      <div>
-      // Add additional attributes here
-        id={this.props.buttons}
+      <div
+        id={this.props.sound}
         className='button'
         onClick={this.props.playSound}
+      >
+
         <p>{this.props.sound}</p>
 
       </div>
